e5bff7ddd0e4765f154be11628e179cc36778f49
einkinner <liu_shihong@sohu.com>
2025-07-27 15:59:15 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,244 @@
+let bleDevice, gattServer;
+let epdService, epdCharacteristic;
+let currentRotate = 0;
+let lastWriteTime = 0;
+let timerId = null;
+
+const timeoutSeconds = 60;
+const textDecoder = new TextDecoder();
+
+const EpdCmd = {
+  INIT:      0x01,
+  SET_TIME:  0x20,
+  SET_ROTATE: 0x21,
+};
+
+function hex2bytes(hex) {
+  for (var bytes = [], c = 0; c < hex.length; c += 2)
+    bytes.push(parseInt(hex.substr(c, 2), 16));
+  return new Uint8Array(bytes);
+}
+
+function bytes2hex(data) {
+  return new Uint8Array(data).reduce(
+    function (memo, i) {
+      return memo + ("0" + i.toString(16)).slice(-2);
+    }, "");
+}
+
+function intToHex(intIn) {
+  let stringOut = ("0000" + intIn.toString(16)).substr(-4)
+  return stringOut.substring(2, 4) + stringOut.substring(0, 2);
+}
+
+function resetVariables() {
+  gattServer = null;
+  epdService = null;
+  epdCharacteristic = null;
+  msgIndex = 0;
+  document.getElementById("log").value = '';
+}
+
+async function write(cmd, data, withResponse = true) {
+  if (!epdCharacteristic) {
+    addLog("服务不可用，请检查蓝牙连接");
+    return false;
+  }
+  let payload = [cmd];
+  if (data) {
+    if (typeof data == 'string') data = hex2bytes(data);
+    if (data instanceof Uint8Array) data = Array.from(data);
+    payload.push(...data)
+  }
+  addLog(bytes2hex(payload), '⇑');
+  try {
+    if (withResponse)
+      await epdCharacteristic.writeValueWithResponse(Uint8Array.from(payload));
+    else
+      await epdCharacteristic.writeValueWithoutResponse(Uint8Array.from(payload));
+  } catch (e) {
+    console.error(e);
+    if (e.message) addLog("write: " + e.message);
+    return false;
+  }
+  lastWriteTime = new Date().getTime();
+  return true;
+}
+
+async function syncTime(mode) {
+  const timestamp = new Date().getTime() / 1000;
+  const data = new Uint8Array([
+    (timestamp >> 24) & 0xFF,
+    (timestamp >> 16) & 0xFF,
+    (timestamp >> 8) & 0xFF,
+    timestamp & 0xFF,
+    -(new Date().getTimezoneOffset() / 60),
+    mode
+  ]);
+  if (await write(EpdCmd.SET_TIME, data)) {
+    addLog("时间已同步！");
+    addLog("屏幕刷新完成前请不要操作。");
+  }
+}
+
+async function rotateScreen() {
+  const rotate = (currentRotate === 0) ? 2 : 0;
+  await write(EpdCmd.SET_ROTATE, [rotate]);
+  currentRotate = rotate;
+  addLog("屏幕已旋转180°，正在刷新...");
+  syncTime(1);
+}
+
+function updateButtonStatus() {
+  const connected = gattServer != null && gattServer.connected;
+  const status = connected ? null : 'disabled';
+  document.getElementById("btn-reconnect").disabled = (gattServer == null || gattServer.connected) ? 'disabled' : null;
+  document.getElementById("btn-synctime").disabled = status;
+  document.getElementById("btn-rotate").disabled = status;
+}
+
+function disconnect() {
+  updateButtonStatus();
+  resetVariables();
+  addLog('已断开连接.');
+  document.getElementById("btn-connect").innerHTML = '连接';
+}
+
+async function preConnect() {
+  if (gattServer != null && gattServer.connected) {
+    if (bleDevice != null && bleDevice.gatt.connected) {
+      bleDevice.gatt.disconnect();
+    }
+  }
+  else {
+    resetVariables();
+    try {
+      bleDevice = await navigator.bluetooth.requestDevice({
+        optionalServices: ['62750001-d828-918d-fb46-b6c11c675aec'],
+        acceptAllDevices: true,
+      });
+    } catch (e) {
+      console.error(e);
+      if (e.message) addLog("requestDevice: " + e.message);
+      addLog("请检查蓝牙是否已开启，且使用的浏览器支持蓝牙！建议使用以下浏览器：");
+      addLog("• 电脑: Chrome/Edge");
+      addLog("• Android: Chrome/Edge");
+      addLog("• iOS: Bluefy 浏览器");
+      return;
+    }
+
+    await bleDevice.addEventListener('gattserverdisconnected', disconnect);
+    setTimeout(async function () { await connect(); }, 300);
+  }
+}
+
+async function reConnect() {
+  if (bleDevice != null && bleDevice.gatt.connected)
+    bleDevice.gatt.disconnect();
+  resetVariables();
+  addLog("正在重连");
+  setTimeout(async function () { await connect(); }, 300);
+}
+
+function handleNotify(value, idx) {
+  const data = new Uint8Array(value.buffer, value.byteOffset, value.byteLength);
+  const msg = textDecoder.decode(data);
+  addLog(bytes2hex(data), '⇓');
+  if (idx == 0) {
+    if (data.length > 12) currentRotate = data[12];
+  } else {
+    if (msg.startsWith('t=') && msg.length > 2) {
+      const t = parseInt(msg.substring(2)) + new Date().getTimezoneOffset() * 60;
+      addLog(`远端时间: ${new Date(t * 1000).toLocaleString()}`);
+      addLog(`本地时间: ${new Date().toLocaleString()}`);
+    }
+  }
+}
+
+function timeoutHandler() {
+  if (gattServer == null || !gattServer.connected) {
+    clearInterval(timerId);
+    timerId = null;
+    return;
+  }
+
+  const now = new Date().getTime();
+  if (now - lastWriteTime > timeoutSeconds * 1000) {
+    addLog(`${timeoutSeconds} 秒无操作，断开连接...`);
+    if (bleDevice != null && bleDevice.gatt.connected) {
+      bleDevice.gatt.disconnect();
+    }
+  }
+}
+
+async function connect() {
+  if (bleDevice == null || epdCharacteristic != null) return;
+
+  try {
+    addLog("正在连接: " + bleDevice.name);
+    gattServer = await bleDevice.gatt.connect();
+    addLog('  找到 GATT Server');
+    epdService = await gattServer.getPrimaryService('62750001-d828-918d-fb46-b6c11c675aec');
+    addLog('  找到 EPD Service');
+    epdCharacteristic = await epdService.getCharacteristic('62750002-d828-918d-fb46-b6c11c675aec');
+    addLog('  找到 Characteristic');
+  } catch (e) {
+    console.error(e);
+    if (e.message) addLog("connect: " + e.message);
+    disconnect();
+    return;
+  }
+
+  try {
+    await epdCharacteristic.startNotifications();
+    epdCharacteristic.addEventListener('characteristicvaluechanged', (event) => {
+      handleNotify(event.target.value, msgIndex++);
+    });
+  } catch (e) {
+    console.error(e);
+    if (e.message) addLog("startNotifications: " + e.message);
+  }
+
+  await write(EpdCmd.INIT);
+
+  document.getElementById("btn-connect").innerHTML = '断开';
+  updateButtonStatus();
+  timerId = setInterval(function () { timeoutHandler(); }, 1000);
+}
+
+function addLog(logTXT, action = '') {
+  const log = document.getElementById("log");
+  const now = new Date();
+  const time = String(now.getHours()).padStart(2, '0') + ":" +
+    String(now.getMinutes()).padStart(2, '0') + ":" +
+    String(now.getSeconds()).padStart(2, '0') + " ";
+
+  const logEntry = document.createElement('div');
+  const timeSpan = document.createElement('span');
+  timeSpan.className = 'time';
+  timeSpan.textContent = time;
+  logEntry.appendChild(timeSpan);
+
+  if (action !== '') {
+    const actionSpan = document.createElement('span');
+    actionSpan.className = 'action';
+    actionSpan.innerHTML = action;
+    logEntry.appendChild(actionSpan);
+  }
+  logEntry.appendChild(document.createTextNode(logTXT));
+
+  log.appendChild(logEntry);
+  log.scrollTop = log.scrollHeight;
+
+  while (log.childNodes.length > 20) {
+    log.removeChild(log.firstChild);
+  }
+}
+
+function clearLog() {
+  document.getElementById("log").innerHTML = '';
+}
+
+document.body.onload = () => {
+  updateButtonStatus();
+}
